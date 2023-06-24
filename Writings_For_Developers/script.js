1381b214e0333ff5ed18c2ece34ea6dd530f1acc
Berat Aydın <aydinberat352@gmail.com>
2023-06-24 18:30:13 +0300

    delete

--- Writings_For_Developers/script.js
+++
@@ -1,8 +0,0 @@
-function toggleNavbar() {
-    var navbar = document.getElementById("navbar_right");
-    if (navbar.style.visibility === "visible") {
-        navbar.style.visibility = "hidden";
-    } else {
-        navbar.style.visibility = "visible";
-    }
-}
